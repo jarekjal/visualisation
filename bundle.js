dab10d7d32f13647f36d75dcb7fd33c5e78d8740
jarekjal <jarekjal@wp.pl>
2021-11-27 15:25:31 +0100

    ignorowane pliki

--- bundle.js
+++ bundle.js
@@ -1,14 +1,21 @@
 (function (React, ReactDOM) {
-    'use strict';
+  'use strict';
 
-    function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+  function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
-    var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
-    var ReactDOM__default = /*#__PURE__*/_interopDefaultLegacy(ReactDOM);
+  var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+  var ReactDOM__default = /*#__PURE__*/_interopDefaultLegacy(ReactDOM);
 
-    const App = () => /*#__PURE__*/React__default["default"].createElement("h1", null, "Hello JSX");
+  const App = () => /*#__PURE__*/React__default["default"].createElement("h1", null, "Hello JSX");
 
-    const rootElement = document.getElementById('root');
-    ReactDOM__default["default"].render( /*#__PURE__*/React__default["default"].createElement(App, null), rootElement);
+  function MojKomponent() {
+    return /*#__PURE__*/React__default["default"].createElement("div", null, /*#__PURE__*/React__default["default"].createElement("dialog", {
+      open: true
+    }, /*#__PURE__*/React__default["default"].createElement("h1", null, "Tytu\u0142"), /*#__PURE__*/React__default["default"].createElement("p", null, "Tre\u015B\u0107")));
+  }
+
+  const rootElement = document.getElementById('root');
+  ReactDOM__default["default"].render( /*#__PURE__*/React__default["default"].createElement(App, null), rootElement);
+  ReactDOM__default["default"].render( /*#__PURE__*/React__default["default"].createElement(MojKomponent, null), document.getElementById('root2'));
 
 })(React, ReactDOM);
