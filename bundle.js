bfe638e6cde8cf528441f51727ed1d515f63c7a7
jarekjal <jarekjal@wp.pl>
2021-11-28 14:18:29 +0100

    svg w jsx

--- bundle.js
+++ bundle.js
@@ -1,21 +1,42 @@
 (function (React, ReactDOM) {
-  'use strict';
+    'use strict';
 
-  function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+    function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
-  var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
-  var ReactDOM__default = /*#__PURE__*/_interopDefaultLegacy(ReactDOM);
+    var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+    var ReactDOM__default = /*#__PURE__*/_interopDefaultLegacy(ReactDOM);
 
-  const App = () => /*#__PURE__*/React__default["default"].createElement("h1", null, "Hello JSX");
+    const height = 600;
+    const width = 600;
+    const centreX = width / 2;
+    const centreY = height / 2;
+    const strokeWidth = 10;
+    const eyeOffset = 100;
+    const eyeRadius = 50;
 
-  function MojKomponent() {
-    return /*#__PURE__*/React__default["default"].createElement("div", null, /*#__PURE__*/React__default["default"].createElement("dialog", {
-      open: true
-    }, /*#__PURE__*/React__default["default"].createElement("h1", null, "Tytu\u0142"), /*#__PURE__*/React__default["default"].createElement("p", null, "Tre\u015B\u0107")));
-  }
+    const App = () => /*#__PURE__*/React__default["default"].createElement("svg", {
+      height: height,
+      width: width
+    }, /*#__PURE__*/React__default["default"].createElement("circle", {
+      cx: centreX,
+      cy: centreY,
+      r: (width - strokeWidth) / 2,
+      stroke: "black",
+      "stroke-width": strokeWidth,
+      fill: "yellow"
+    }), /*#__PURE__*/React__default["default"].createElement("circle", {
+      cx: centreX - eyeOffset,
+      cy: centreY - eyeOffset,
+      r: eyeRadius,
+      fill: "black"
+    }), /*#__PURE__*/React__default["default"].createElement("circle", {
+      cx: centreX + eyeOffset,
+      cy: centreY - eyeOffset,
+      r: eyeRadius,
+      fill: "black"
+    }));
 
-  const rootElement = document.getElementById('root');
-  ReactDOM__default["default"].render( /*#__PURE__*/React__default["default"].createElement(App, null), rootElement);
-  ReactDOM__default["default"].render( /*#__PURE__*/React__default["default"].createElement(MojKomponent, null), document.getElementById('root2'));
+    const rootElement = document.getElementById('root');
+    ReactDOM__default["default"].render( /*#__PURE__*/React__default["default"].createElement(App, null), rootElement);
 
 })(React, ReactDOM);
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const height = 600;
+const width = 600;
+const centreX = width/2;
+const centreY = height/2;
+const strokeWidth = 10;
+const eyeOffset = 100;
+const eyeRadius = 50;
 
 const App = () => (
-    <h1>Hello JSX</h1>
+    <svg height={height} width={width}>
+        <circle cx={centreX} cy={centreY} r={(width - strokeWidth)/2} stroke="black" stroke-width={strokeWidth} fill="yellow" />
+        <circle cx={centreX - eyeOffset} cy={centreY - eyeOffset} r={eyeRadius} fill="black" />
+        <circle cx={centreX + eyeOffset} cy={centreY - eyeOffset} r={eyeRadius} fill="black" />
+    </svg>
 ); 
 
 const rootElement = document.getElementById('root');
